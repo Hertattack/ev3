2659803edc0e3face5be5b8cb6f93b48d23a50f9
Marcel <marcel.schot@gmail.com>
2022-02-21 22:22:53 +0100

    Test ultrasonic sensor

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-from ev3dev2.sound import Sound
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import UltrasonicSensor
 
-Sound().beep()
+us = UltrasonicSensor(INPUT_2)
+
+us.mode = us.MODE_US_DIST_CM
+print(us.distance_centimeters);
+
+
