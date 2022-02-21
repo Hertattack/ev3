36937ddb33a212d8a9d00bc193c729c8bf76691c
Marcel <marcel.schot@gmail.com>
2022-02-21 22:41:43 +0100

    Configure port

--- src/python/main.py
+++ src/python/main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+from time import sleep
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.port import LegoPort
 from ev3dev2.sensor.lego import UltrasonicSensor
+
+p2 = LegoPort(INPUT_2)
+p2.mode = 'nxt-i2c'
+p2.set_device = 'lego-nxt-us'
+
+sleep(0.5)
 
 us = UltrasonicSensor(INPUT_2)
 
 us.mode = us.MODE_US_DIST_CM
-print(us.distance_centimeters);
+print(us.distance_centimeters)
 
 
