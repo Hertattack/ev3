ba7c10bff6a3510a5ea4b9e147161e37f4b4ac60
Marcel <marcel.schot@gmail.com>
2022-02-27 12:29:49 +0100

    Add check if exceptions are raised for improper alert message

--- src/python/test/protocol/messages/AlertMessage_test.py
+++ src/python/test/protocol/messages/AlertMessage_test.py
@@ -1,4 +1,5 @@
 import pytest
+from src.poweredup.protocol import ProtocolError
 from src.poweredup.protocol.messages import Message, MessageType
 from src.poweredup.protocol.alerts import AlertMessage, AlertOperation, AlertStatus
 
@@ -13,3 +14,9 @@ def test_alert_message_without_payload(alert_type: bytes, alert_operation: bytes
     assert alert_message.alert_type.value == alert_type
     assert alert_message.operation.value == alert_operation
     assert alert_message.value == expected_value
+
+
+def test_alert_message_unsupported_type():
+    message_bytes = b'\x05\x00\x03\x44\x04'
+    with pytest.raises(ProtocolError, match=r"Value .* is not supported in mapping."):
+        Message.parse_bytes(message_bytes)
