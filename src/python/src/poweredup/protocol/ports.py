9084a33dc44aa61a24902d4d54b188e621576403
Marcel <marcel.schot@gmail.com>
2022-03-02 19:43:45 +0100

    Implement port input format setup single

--- src/python/src/poweredup/protocol/ports.py
+++ src/python/src/poweredup/protocol/ports.py
@@ -67,7 +67,13 @@ class PortModeInformationRequestMessage(Message):
 
     @classmethod
     def parse_bytes(cls, message_bytes: bytes):
-        pass
+        message_length = len(message_bytes)
+        if message_length != 3:
+            raise ProtocolError("Message too long")
+
+        portId = PortID(message_bytes[0:1])
+        modeInformation = ModeInformationType(message_bytes[2:])
+        return PortModeInformationRequestMessage(portId, message_bytes[1:2], modeInformation)
 
     def __init__(self, port_id: PortID, mode: bytes, mode_information: ModeInformationType):
         self.port_id = port_id
@@ -76,5 +82,58 @@ class PortModeInformationRequestMessage(Message):
 
     @property
     def value(self):
-        header = CommonMessageHeader(3,self.MESSAGE_TYPE)
+        header = CommonMessageHeader(3, self.MESSAGE_TYPE)
         return header.value + self.port_id.value + self.mode + self.mode_information.value
+
+
+class PortInputFormatSetupSingle(Message):
+    MESSAGE_TYPE = MessageType.PORT_INPUT_FORMAT_SETUP_SINGLE
+
+    @classmethod
+    def parse_bytes(cls, message_bytes: bytes):
+        message_length = len(message_bytes)
+        if message_length != 7:
+            raise ProtocolError("Expected length of 6 bytes for message payload.")
+
+        port_id = PortID(message_bytes[0:1])
+        mode = message_bytes[1:2]
+        delta_interval = message_bytes[2:6]
+        notification_enabled = int.from_bytes(message_bytes[6:], byteorder="big", signed=False)
+
+        return PortInputFormatSetupSingle(port_id, mode, delta_interval, notification_enabled)
+
+    def __init__(self, port_id: PortID, mode: bytes, delta_interval: bytes, notification_enabled: int):
+        if len(mode) != 1:
+            raise ProtocolError("Expected length of mode is 1 bytes.")
+
+        if len(delta_interval) != 4:
+            raise ProtocolError("Expected length of delta interval is 4 bytes (uint32)")
+
+        if 0 > notification_enabled > 1:
+            raise ProtocolError("Expected notification value to be 0 = Disabled and 1 = Enabled")
+
+        self.port_id = port_id
+        self.mode = mode
+        self.delta_interval = delta_interval
+        self.notification_enabled = notification_enabled
+
+    @property
+    def value(self):
+        header = CommonMessageHeader(7, self.MESSAGE_TYPE)
+        return header.value + self.port_id.value + self.mode + self.delta_interval + \
+               self.notification_enabled.to_bytes(1, byteorder="big", signed=False)
+
+
+class PortInputFormatSetupCombined(Message):
+    MESSAGE_TYPE = MessageType.PORT_INPUT_FORMAT_SETUP_COMBINED
+
+    @classmethod
+    def parse_bytes(cls, message_bytes: bytes):
+        pass
+
+    def __init__(self):
+        pass
+
+    @property
+    def value(self):
+        pass
