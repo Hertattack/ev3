463e95a4653357c39cc90a7f069193754d9d6b2c
Marcel <marcel.schot@gmail.com>
2023-01-22 12:03:01 +0100

    No interpolation is supported on ev3dev

--- src/python/integration-tests.py
+++ src/python/integration-tests.py
@@ -6,7 +6,7 @@ print("Please specify the integration test to run")
 test_index = 1
 for test_option in available_tests:
     test_name = test_option.name()
-    print(f"{test_index}) {test_name}")
+    print("%i) %s" % (test_index, test_name))
     test_index += 1
 
 choice = input("Your choice?")
@@ -18,11 +18,11 @@ try:
     else:
         test_class = available_tests[option - 1]
 except:
-    print(f"Invalid option: {choice}")
+    print("Invalid option: %s" % choice)
     sys.exit(1)
 
 test_name = test_class.name()
-print(f"Running test {choice}: {test_name}")
+print("Running test %s: %s" % (choice, test_name))
 
 test = test_class()
 test.run(sys.argv)
